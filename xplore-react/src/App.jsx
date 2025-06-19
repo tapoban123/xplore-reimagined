41bd6b54c3e9eb424316a164df077de2e9ffe43b
Subh <subhaushsingh@gmail.com>
2025-06-19 11:18:42 +0530

    initial commit and grid section

--- xplore-react/src/App.jsx
+++ xplore-react/src/App.jsx
@@ -9,7 +9,7 @@ function App() {
 
   return (
     <>
-      <main className='bg-black min-h-screen text-white'>
+      <main className='bg-black min-h-screen'>
         <Hero />
       </main>
     </>
--- xplore-react/src/components/Hero.jsx
+++ xplore-react/src/components/Hero.jsx
@@ -3,7 +3,28 @@ import React from 'react'
 const Hero = () => {
   return (
     <>
-        <div>Test Div</div>
+        <section className='hero-section w-full'>
+          <div className='hero-grid'>
+            <div className='card card-1'>
+              <div className='bg-container'>
+                <div className='x-bg'>X</div>
+                <div className='p-bg'>P</div>
+              </div>
+              <div className='card-content'>
+                <h2>Your</h2>
+                <h2>Future.</h2>
+                <h2>Mapped by</h2>
+                <h2>XPlore</h2>
+                <p>Personalized career guidance, built for the New Gen.</p>
+                <div className='cta-link'>More about Xplore</div>
+              </div>
+              <div className='boy-reading'>
+                <img src="/images/reading-boy.png" alt="Reading Boy" />
+              </div>
+            </div>
+            <div className='card card-2'></div>
+          </div>
+        </section>
     </>
   )
 }
